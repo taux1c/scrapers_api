419d8cdc4a8f8f805dbb01f087d83bbb02995e46
qpidcock <24677318+qpidcock@users.noreply.github.com>
2022-10-02 13:25:51 -0400

    Uploading just a basic structure of my vision of the project.

---
+++ onlyfans/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+
+
+    "endpoints",
+    "highlights",
+    "async_highlights",
+    "messages",
+    "async_messages",
+    "posts",
+    "async_posts",
+    "paid",
+    "async_paid",
+    "meta_cleaner",
+
+
+]
---
+++ onlyfans/endpoints.py
@@ -0,0 +1,26 @@
+
+
+endpoints = {
+
+    "messages":"",
+    "posts":"",
+    "highlights":"",
+    "archived_messages":"",
+    "archived_posts":"",
+    "paid_content":"",
+
+
+}
+
+methods = {
+
+    "messages":"get",
+    "paid_content":"get",
+    "posts":"get",
+    "highlights":"get",
+    "archived_messages":"get",
+    "archived_posts":"get",
+
+
+
+}
